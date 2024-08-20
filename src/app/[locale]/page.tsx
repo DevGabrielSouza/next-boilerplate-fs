ef2dbdc493a6c11a913c35f4150e37afd07e3018
Gabriel Souza <contato@devgabrielsouza.com.br>
2024-08-20 16:15:37 -0300

    chore(intl): add app description

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import { getTranslations } from 'next-intl/server'
+
+export default async function Home() {
+  const t = await getTranslations()
+
   return (
-    <main className='flex bg-violet-900 min-h-screen flex-col items-center justify-center'>
+    <main className='flex bg-violet-900 min-h-screen flex-col items-center justify-center gap-y-2'>
       <h1 className='text-4xl text-white font-bold'>Next Boilerplate</h1>
+      <h2 className='text-white'>{t('app.description')}</h2>
     </main>
   )
 }
