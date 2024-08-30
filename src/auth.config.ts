e5ea27dad3385cb4ed9f44e2a59b31bedf16c116
Gabriel Souza <contato@devgabrielsouza.com.br>
2024-08-30 10:22:58 -0300

    chore(auth): adjust auth lib and add user id to session user

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -4,6 +4,7 @@ import Github from 'next-auth/providers/github'
 import Google from 'next-auth/providers/google'
 import { prisma } from './lib/prisma'
 import credentials from 'next-auth/providers/credentials'
+import { type Session } from 'next-auth'
 
 import { authenticateUser } from './application/services/auth/authenticate-user.service'
 
@@ -22,6 +23,28 @@ export default {
       authorize: authenticateUser
     })
   ],
+
+  callbacks: {
+    async signIn({ user }) {
+      if (!user.id) {
+        return false
+      }
+
+      return true
+    },
+
+    async session({ session, token }: { session: Session; token?: any }) {
+      if (token && token.sub && session.user) {
+        session.user.id = token.sub
+      }
+
+      return session
+    },
+
+    async jwt({ token, user }) {
+      return token
+    }
+  },
 
   adapter: PrismaAdapter(prisma)
 } satisfies NextAuthConfig
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,6 @@
 import { ExtendedUser } from '@/types/next-auth'
 import { auth } from '@/auth'
+import { type Session } from 'next-auth'
 
 export const currentSession = async () => {
   const session = await auth()
@@ -7,16 +8,20 @@ export const currentSession = async () => {
   return session
 }
 
-export const currentUser = async () => {
+export const currentUser = async (): Promise<ExtendedUser | null> => {
   const session = await auth()
 
   return session?.user || null
 }
 
-export const updateUser = async (user: ExtendedUser) => {
+export const updateUser = async (
+  user: ExtendedUser
+): Promise<Session | null> => {
   const session = await auth()
 
-  if (session) {
-    session.user = user
-  }
+  if (!session) return null
+
+  session.user = user
+
+  return session
 }
